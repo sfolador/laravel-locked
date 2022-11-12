8f044e3e9aeab8553ef31382d917df6a2d043f1e
sfolador <sfolador@gmail.com>
2022-11-12 17:29:49 +0100

    fixed tests

--- tests/LockedServiceProviderTest/LockedCustomColumn.php
+++ tests/LockedServiceProviderTest/LockedCustomColumn.php
@@ -3,10 +3,12 @@
 use Sfolador\Locked\Tests\TestClasses\TestModel;
 
 it('can choose a custom column to save locked state', function () {
+    $defaultValue =  config()->get('locked.locking_column');
     config()->set('locked.locking_column', 'custom_locked_at');
     $model = TestModel::factory()->locked()->create();
 
     expect($model->isLocked())->toBeTrue()
         ->and($model->custom_locked_at)->not->toBeNull()
         ->and($model->locked_at)->toBeNull();
+    config()->set('locked.locking_column', $defaultValue);
 });
