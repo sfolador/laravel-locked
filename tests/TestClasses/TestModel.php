4f6bca638effc3e4286e1709b1d4a767a2ff5a66
sfolador <folador@medacta.ch>
2022-11-12 13:33:03 +0100

    added global scopes

--- tests/TestClasses/TestModel.php
+++ tests/TestClasses/TestModel.php
@@ -3,10 +3,18 @@
 namespace Sfolador\Locked\Tests\TestClasses;
 
 use Illuminate\Database\Eloquent\Model;
+use Sfolador\Locked\Scopes\LockedScope;
+use Sfolador\Locked\Scopes\UnlockedScope;
 use Sfolador\Locked\Traits\HasLocks;
-use Sfolador\Locked\Traits\Lockable;
+
 
 class TestModel extends Model
 {
     use HasLocks;
+
+    protected static function booted()
+    {
+       static::addGlobalScope(new LockedScope());
+       static::addGlobalScope(new UnlockedScope());
+    }
 }
