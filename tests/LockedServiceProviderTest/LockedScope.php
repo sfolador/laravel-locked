5565f92115eff8ce616b950488b2bd437589c336
sfolador <sfolador@gmail.com>
2022-11-12 14:10:14 +0100

    added local scopes and tests

--- tests/LockedServiceProviderTest/LockedScope.php
+++ tests/LockedServiceProviderTest/LockedScope.php
@@ -4,10 +4,22 @@ use Sfolador\Locked\Tests\TestClasses\TestModel;
 
 it('can filter a model with a scope to see if it is locked', function () {
     $model = TestModel::factory()->locked()->create();
-
     $model2 = TestModel::factory()->unlocked()->create();
 
     $locked = TestModel::locked()->get();
-    expect($locked->count())->toBe(1);
-    expect($locked->first()->id)->toBe($model->id);
+    expect($locked->count())
+        ->toBe(1)
+        ->and($locked->first()->id)
+        ->toBe($model->id);
+});
+
+it('can filter a model with a scope to see if it is unlocked', function () {
+    $model = TestModel::factory()->locked()->create();
+    $model2 = TestModel::factory()->unlocked()->create();
+
+    $locked = TestModel::unlocked()->get();
+    expect($locked->count())
+        ->toBe(1)
+        ->and($locked->first()->id)
+        ->toBe($model2->id);
 });
--- tests/TestClasses/TestModel.php
+++ tests/TestClasses/TestModel.php
@@ -4,8 +4,6 @@ namespace Sfolador\Locked\Tests\TestClasses;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Sfolador\Locked\Scopes\LockedScope;
-use Sfolador\Locked\Scopes\UnlockedScope;
 use Sfolador\Locked\Tests\database\factories\TestModelFactory;
 use Sfolador\Locked\Traits\HasLocks;
 
@@ -14,11 +12,6 @@ class TestModel extends Model
     use HasLocks;
     use HasFactory;
 
-    protected static function booted()
-    {
-        static::addGlobalScope(new LockedScope());
-        static::addGlobalScope(new UnlockedScope());
-    }
 
     protected static function newFactory()
     {
