398790f491e7d6f184a95989e5c2ed5aa986d0ed
sfolador <sfolador@gmail.com>
2022-11-12 13:49:29 +0100

    added TestModelFactory.php

---
+++ tests/database/factories/TestModelFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sfolador\Locked\Tests\database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Sfolador\Locked\Tests\TestClasses\TestModel;
+
+class TestModelFactory extends Factory
+{
+    protected $model = TestModel::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+
+    public function locked(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'locked_at' => now(),
+            ];
+        });
+    }
+
+    public function unlocked(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'locked_at' => null,
+            ];
+        });
+    }
+}
