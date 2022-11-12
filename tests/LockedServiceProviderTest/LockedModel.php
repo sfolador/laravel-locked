45d5acf24ae4304cf0f089f4273ad333460a5a26
sfolador <sfolador@gmail.com>
2022-11-12 13:33:03 +0100

    added global scopes

--- tests/LockedServiceProviderTest/LockedModel.php
+++ tests/LockedServiceProviderTest/LockedModel.php
@@ -6,4 +6,21 @@ it('can be locked', function () {
    $model = new TestModel();
    $model->lock();
    expect($model->isLocked())->toBeTrue();
+   expect($model->isNotUnlocked())->toBeTrue();
+});
+
+it('can be unlocked', function () {
+    $model = new TestModel();
+    $model->lock();
+    $model->unlock();
+    expect($model->isUnlocked())->toBeTrue();
+    expect($model->isNotLocked())->toBeTrue();
+});
+
+it('can be locked or unlocked with a toggle', function () {
+    $model = new TestModel();
+    $model->toggleLock();
+    expect($model->isLocked())->toBeTrue();
+    $model->toggleLock();
+    expect($model->isUnlocked())->toBeTrue();
 });
---
+++ tests/LockedServiceProviderTest/LockedScope.php
@@ -0,0 +1,7 @@
+<?php
+
+use Sfolador\Locked\Tests\TestClasses\TestModel;
+
+it('can filter a model with a scope to see if it is locked', function () {
+
+});
