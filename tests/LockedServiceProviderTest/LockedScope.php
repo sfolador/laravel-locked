9d711762a10bf3d5b6c5142cd307e716e5c8ea57
sfolador <sfolador@gmail.com>
2022-11-12 14:08:42 +0100

    removed global scopes because not useful

--- tests/LockedServiceProviderTest/LockedScope.php
+++ tests/LockedServiceProviderTest/LockedScope.php
@@ -1,4 +1,13 @@
 <?php
 
+use Sfolador\Locked\Tests\TestClasses\TestModel;
+
 it('can filter a model with a scope to see if it is locked', function () {
+    $model = TestModel::factory()->locked()->create();
+
+    $model2 = TestModel::factory()->unlocked()->create();
+
+    $locked = TestModel::locked()->get();
+    expect($locked->count())->toBe(1);
+    expect($locked->first()->id)->toBe($model->id);
 });
