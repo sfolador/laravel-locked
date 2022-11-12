9364ebbcb262ec2d0fafeae1e53241d897088709
sfolador <folador@medacta.ch>
2022-11-12 14:38:12 +0100

    added test for LockedCommand.php

--- src/Commands/LockedCommand.php
+++ src/Commands/LockedCommand.php
@@ -8,11 +8,16 @@ class LockedCommand extends Command
 {
     public $signature = 'lock:add {model}';
 
-    public $description = 'Add lock to a model';
+    public $description = 'Add a migration to add locked columns to a model';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        //verify that the model type exists
+        $model = $this->argument('model');
+        if (!class_exists($model)) {
+            $this->error("Model $model does not exist");
+            return 1;
+        }
 
         return self::SUCCESS;
     }
--- tests/LockedServiceProviderTest/LockedCommand.php
+++ tests/LockedServiceProviderTest/LockedCommand.php
@@ -1,3 +1,14 @@
 <?php
 
-it('can create a migration for locking a model');
+use Sfolador\Locked\Tests\TestClasses\TestModel;
+
+it('cannot invoke the command for a model that does not exist',function (){
+    $this->artisan('lock:add', ['model' => 'App\Models\NotexistingModel'])
+        ->expectsOutput('Model App\Models\NotexistingModel does not exist')
+        ->assertExitCode(1);
+});
+
+it('can invoke the command for a model that exists',function (){
+    $this->artisan('lock:add', ['model' => TestModel::class])
+        ->assertExitCode(0);
+});
